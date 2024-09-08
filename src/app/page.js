fcbd303cf2f316fef65e4b24a2a2254e352fbed1
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-09-08 21:46:40 +0500

    Remove Bug Of Tailwindcs Not added to Component Folder Soo i change the  component to Component

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import { webSiteHeadingFont } from "./layout";
 export default function Home() {
   return (
    <>
-    <main className="md:h-[80vh] h-[100vh] relative mt-[-60px] ">
+    <main className="md:h-[80vh] h-[100vh] relative mt-[-60px] -z-10">
       <div className=" mainBg relative w-full h-full bg-black block"></div>
-      <div className=" main-content absolute w-full h-full top-0 left-0 bg-transparent ">
+      <div className=" main-content absolute w-full h-full top-0 left-0 bg-transparent z-10 ">
         <div className="container mx-auto px-2 w-full h-full flex-col gap-6 flex items-center justify-center">
           <h1 className={`md:text-5xl text-4xl  text-bold text-white text-center ${webSiteHeadingFont.className}`}>
             Unlock knowledge, embrace growth—every step counts
@@ -16,7 +16,7 @@ export default function Home() {
             WELCOME TO GH HELPER
           </h2>
           <p className="text-center text-gray-200 md:w-[70%] w-[95%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam sunt ea repellat dicta sit et suscipit dolore velit ipsa? Quos natus eveniet quaerat repudiandae necessitatibus dicta vel quisquam ducimus.</p>
-          <button className="z-20 md:mt-9 mt-2 mx-auto px-4 py-3 font-bold text-white cursor-pointer bg-purple-800 rounded-lg hover:shadow-md hover:shadow-cyan-500 hover:outline-2 hover:outline-cyan-400">Explorer</button>
+          <button className="z-50 md:mt-9 mt-2 mx-auto px-4 py-3 font-bold text-white cursor-pointer bg-purple-800 rounded-lg hover:shadow-md hover:shadow-cyan-500 hover:outline-2 hover:outline-cyan-400">Explorer</button>
         </div>
       </div>
     </main>
