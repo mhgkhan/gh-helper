53000caa1f92bbc444017b3a47fddfb9ec098954
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-09-09 21:56:28 +0500

    Fixed Some UI Issues

--- src/Components/UI/Header.jsx
+++ src/Components/UI/Header.jsx
@@ -3,12 +3,14 @@
 
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
-
+import { FiAlignCenter, FiAlignJustify } from "react-icons/fi"
+import {GrClose } from  "react-icons/gr"
 
 
 const Header = ({ font }) => {
 
     const [isMobile, setIsMobile] = useState(false)
+    const [sidebarOpened,setSidebarOpened] = useState(false)
 
     const changeHeaderResponsive = () => {
         if (window.innerWidth < 600) {
@@ -34,35 +36,37 @@ const Header = ({ font }) => {
 
     return (
         <header className='z-[10000]'>
-            <div className={`container mx-auto  bg-transparent flex ${isMobile? "flex-col h-[60px] overflow-hidden":"items-end justify-around h-[60px]"}`}>
-               
-               <div className="logo-section flex items-end justify-between">
-               <div className={`logo flex items-end justify-center gap-1  ${font.className}`}>
-                    <h1 className='text-5xl text-pink-600'>G </h1>
-                    <h1 className='text-5xl text-white'>Helper  </h1>
+            <div className={`container mx-auto  bg-transparent transition-all duration-300 flex ${isMobile ? `flex-col h-[60px] ${sidebarOpened?`h-[300px]`: `h-[60px]`} overflow-hidden` : "items-end justify-around h-[60px]"}`}>
+
+                <div className="logo-section flex items-end justify-between md:px-0 px-3 ">
+                    <div className={`logo flex items-end justify-center gap-1  ${font.className}`}>
+                        <h1 className='text-5xl text-pink-600'>G </h1>
+                        <h1 className='text-5xl text-white'>Helper  </h1>
+                    </div>
+                   {
+                    isMobile ?  <button className='text-4xl text-white bg-pink-300 rounded-md p-1' onClick={()=>setSidebarOpened(!sidebarOpened)}>
+                    {sidebarOpened?  < GrClose/> :   <FiAlignJustify />}
+                  </button>  : ""
+                   }
                 </div>
-                <button>
-                
-                </button>
-               </div>
 
 
-                <nav className={`${isMobile ? "mt-3":"mt-0"}`}>
-                    <ul className={`flex ${isMobile?"items-center justify-center flex-col":"items-center justify-center"} gap-2`}>
-                        <li className='transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1'>
-                            <Link className='text-white font-bold text-lg  ' href={"/"} >HOME</Link>
+                <nav className={`${isMobile ? "mt-3" : "mt-0"} ` }>
+                    <ul className={`flex ${isMobile ? "items-center justify-center flex-col" : "items-center justify-center"} gap-2`}>
+                        <li className={` ${isMobile ? "w-full mt-[4px] text-center" : "w-auto"} transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1`}>
+                            <Link className={`text-white transition-all duration-200 font-bold text-lg ${isMobile ? "w-full hover:bg-slate-500 block p-1" : "w-auto"}  `} href={"/"} >HOME</Link>
                         </li>
-                        <li className='transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1'>
-                            <Link className='text-white font-bold text-lg  ' href={"/"} >BLOGS </Link>
+                        <li className={` ${isMobile ? "w-full mt-[4px] text-center" : "w-auto"} transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1`}>
+                            <Link className={`text-white transition-all duration-200 font-bold text-lg ${isMobile ? "w-full hover:bg-slate-500 block p-1" : "w-auto"}  `} href={"/"} >BLOGS </Link>
                         </li>
-                        <li className='transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1'>
-                            <Link className='text-white font-bold text-lg  ' href={"/"} >TRENDING </Link>
+                        <li className={` ${isMobile ? "w-full mt-[4px] text-center" : "w-auto"} transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1`}>
+                            <Link className={`text-white transition-all duration-200 font-bold text-lg ${isMobile ? "w-full hover:bg-slate-500 block p-1" : "w-auto"}  `} href={"/"} >TRENDING </Link>
                         </li>
-                        <li className='transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1'>
-                            <Link className='text-white font-bold text-lg  ' href={"/"} >ABOUT</Link>
+                        <li className={` ${isMobile ? "w-full mt-[4px] text-center" : "w-auto"} transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1`}>
+                            <Link className={`text-white transition-all duration-200 font-bold text-lg ${isMobile ? "w-full hover:bg-slate-500 block p-1" : "w-auto"}  `} href={"/"} >ABOUT</Link>
                         </li>
-                        <li className='transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1'>
-                            <Link className='text-white font-bold text-lg  ' href={"/"} >CONTACT </Link>
+                        <li className={` ${isMobile ? "w-full mt-[4px] text-center" : "w-auto"} transition duration-500  hover:border-b-2 hover:border-b-sky-300 px-1`}>
+                            <Link className={`text-white transition-all duration-200 font-bold text-lg ${isMobile ? "w-full hover:bg-slate-500 block p-1" : "w-auto"}  `} href={"/"} >CONTACT </Link>
                         </li>
 
                     </ul>
--- src/Components/UI/home/cards/BlogCard.jsx
+++ src/Components/UI/home/cards/BlogCard.jsx
@@ -1,13 +1,14 @@
 import { subHeadingFont } from '@/app/layout'
+import Image from 'next/image'
 import React from 'react'
 
-const BlogCard = () => {
+const BlogCard = ({img}) => {
     return (
-        <div className='shadow-md shadow-slate-300 md:w-[450px] sm:w-full rounded-md border-3'>
-            <div className="top bg-blue-500 h-[200px] w-full rounded-md"></div>
+        <div className='shadow-md shadow-slate-300 w-[300px] md:h-[450px] h-auto rounded-md border-3'>
+            <div className="top bg-blue-500 h-[200px] w-full rounded-md"> <Image src={img} width={300} height={200} alt='blog pic' /> </div>
             <div className="card-content p-2">
-                <h2 className={` ${subHeadingFont.className} text-3xl font-bold my-3`}>Title of Latest Blog </h2>
-                <p className='text-justify'> excepturi quo marendis impedit enim modi exercitationem. Obcaecati nemo nam laborum quam debitis voluptate cupiditate quae id accusamus esse impedit rerum, corporis expedita ipsam! Minus, aliquam amet, cum dolor architecto quisquam unde impedit blanditiis doloribus cupiditate earum magni. Itaque amet voluptatem impedit mollitia pariatur officia, architecto, quia necessitatibus accusantium nostrum commodi! Dolorum ipsa commodi explicabo distinctio exercitationem, adipisci voluptates, facilis vel sapiente, incidunt quam.</p>
+                <h2 className={` ${subHeadingFont.className} text-2xl font-bold my-3`}>Title of Latest Blog </h2>
+                <p className='text-justify text-gray-600'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo sequi sed, quo dolorem vero perspiciatis recusandae libero. Omnis, ducimus pariatur. Inventore, quam! </p>
                 <button className="text-center bg-blue-500 py-2 px-3 mt-3 rounded-lg text-white">Learn more </button>
             </div>
         </div>
