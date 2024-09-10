50d02a555e5a562ae88c03885783169c2ec7ca3d
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2024-09-10 22:01:28 +0500

    Fixes Header & intro Design Issues

--- src/Components/UI/home/cards/BlogCard.jsx
+++ src/Components/UI/home/cards/BlogCard.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const BlogCard = ({img}) => {
     return (
         <div className='shadow-md shadow-slate-300 w-[300px] md:h-[450px] h-auto rounded-md border-3'>
-            <div className="top bg-blue-500 h-[200px] w-full rounded-md"> <Image src={img} width={300} height={200} alt='blog pic' /> </div>
+            <div className="top bg-blue-500 h-[200px] w-full rounded-md"> <Image className='rounded-md' src={img} width={300} height={200} alt='blog pic' /> </div>
             <div className="card-content p-2">
                 <h2 className={` ${subHeadingFont.className} text-2xl font-bold my-3`}>Title of Latest Blog </h2>
                 <p className='text-justify text-gray-600'> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo sequi sed, quo dolorem vero perspiciatis recusandae libero. Omnis, ducimus pariatur. Inventore, quam! </p>
